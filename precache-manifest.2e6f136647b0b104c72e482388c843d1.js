1e2fe6cdc7ce470da5fe881b9563707279bcf012
ikee <blazej.wodecki@icloud.com>
2022-06-07 14:47:11 +0200

    Updates

---
+++ precache-manifest.2e6f136647b0b104c72e482388c843d1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e23875792fe5ebb3dadf",
+    "url": "/static/css/main.c6544e4b.chunk.css"
+  },
+  {
+    "revision": "e23875792fe5ebb3dadf",
+    "url": "/static/js/main.e2387579.chunk.js"
+  },
+  {
+    "revision": "eb4274499f4cc67658cb",
+    "url": "/static/js/1.eb427449.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f69d423f520b5aaf536687f425556725",
+    "url": "/index.html"
+  }
+];
